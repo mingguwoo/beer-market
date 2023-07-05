25dc30b10e49edd0330805d090c80520a1ab0da5
Xiaozhou TU 涂小洲 <xiaozhou.tu@nio.com>
2023-07-05 15:21:13 +0800

    Maven pom重复清理

--- plm-bom-configuration-infrastructure/src/main/java/com/nio/ngfs/plm/bom/configuration/infrastructure/repository/entity/BaseEntity.java
+++ plm-bom-configuration-infrastructure/src/main/java/com/nio/ngfs/plm/bom/configuration/infrastructure/repository/entity/BaseEntity.java
@@ -4,7 +4,6 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -22,21 +21,18 @@ public class BaseEntity implements Serializable {
     @Serial
     private static final long serialVersionUID = 5862727978698906376L;
 
-    @Schema(description = "id", required = true)
     @TableId(value = "id", type = IdType.AUTO)
     private Long id;
 
     /**
      * 删除状态
      */
-    @Schema(description = "是否删除", required = true)
     @TableLogic(value = "0", delval = "1")
     private Integer delFlag;
 
     /**
      * 创建时间
      */
-    @Schema(description = "创建时间", required = true)
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date createTime;
@@ -44,7 +40,6 @@ public class BaseEntity implements Serializable {
     /**
      * 更新时间
      */
-    @Schema(description = "更新时间")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date updateTime;
@@ -52,13 +47,11 @@ public class BaseEntity implements Serializable {
     /**
      * 创建人
      */
-    @Schema(description = "创建人")
     private String createUser;
 
     /**
      * 更新人
      */
-    @Schema(description = "更新人")
     private String updateUser;
 
 }

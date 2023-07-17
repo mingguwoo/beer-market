63a97208d846c2c56a8a03cc00c9cd478d0567fa
Xiaozhou TU 涂小洲 <xiaozhou.tu@nio.com>
2023-07-17 11:20:22 +0800

    启动报错修复

--- plm-bom-configuration-api/src/main/java/com/nio/ngfs/plm/bom/configuration/api/BomConfigurationApplication.java
+++ plm-bom-configuration-api/src/main/java/com/nio/ngfs/plm/bom/configuration/api/BomConfigurationApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -16,13 +15,12 @@ import org.springframework.scheduling.annotation.EnableAsync;
  * @author luke.zhu
  */
 @EnableDynamicTp
-@EnableFeignClients(basePackages = "com.nio.ngfs")
+@EnableFeignClients(basePackages = {"com.nio.ngfs", "com.nio.ps.third.party"})
 @EnableDiscoveryClient
 @EnableAsync
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
 @OpenAPIDefinition(servers = {@Server(url = "/", description = "BOM Configuration application default URL")})
-@SpringBootApplication(scanBasePackages = {"com.nio.ngfs", "org.springdoc.webmvc.ui","com.nio.ps.third"})
-@ComponentScan("com.nio.ps.third")
+@SpringBootApplication(scanBasePackages = {"com.nio.ngfs", "org.springdoc.webmvc.ui"})
 public class BomConfigurationApplication {
 
     public static void main(String[] args) {

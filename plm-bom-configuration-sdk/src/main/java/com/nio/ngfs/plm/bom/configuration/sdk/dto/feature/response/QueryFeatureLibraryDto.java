9c4c4ddeec77614c9970771a325aea1ba84a6ed1
Xiaozhou TU 涂小洲 <xiaozhou.tu@nio.com>
2023-07-21 17:14:10 +0800

    完成FeatureLibrary列表查询接口

--- plm-bom-configuration-sdk/src/main/java/com/nio/ngfs/plm/bom/configuration/sdk/dto/feature/response/QueryFeatureLibraryDto.java
+++ plm-bom-configuration-sdk/src/main/java/com/nio/ngfs/plm/bom/configuration/sdk/dto/feature/response/QueryFeatureLibraryDto.java
@@ -2,7 +2,9 @@ package com.nio.ngfs.plm.bom.configuration.sdk.dto.feature.response;
 
 import com.nio.ngfs.plm.bom.configuration.sdk.dto.common.Dto;
 import lombok.Data;
+import org.apache.commons.collections4.CollectionUtils;
 
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -38,6 +40,60 @@ public class QueryFeatureLibraryDto implements Dto {
 
     private String updateTime;
 
-    private List<QueryFeatureLibraryDto> children;
+    private List<QueryFeatureLibraryDto> children = Collections.emptyList();
+
+    /**
+     * 子节点列表是否为空
+     */
+    private transient boolean childrenEmpty = false;
+
+    /**
+     * 是否搜索匹配
+     */
+    private transient boolean searchMatch = true;
+
+    public boolean isMatchResult() {
+        return !childrenEmpty && searchMatch;
+    }
+
+    public void checkChildrenEmpty() {
+        if (CollectionUtils.isEmpty(children)) {
+            this.childrenEmpty = true;
+        } else {
+            this.childrenEmpty = children.stream().allMatch(QueryFeatureLibraryDto::isChildrenEmpty);
+        }
+    }
+
+    /**
+     * 匹配搜索词
+     *
+     * @param superMatch 上级是否匹配
+     * @return 下级是否匹配
+     */
+    public boolean selectSearchMatch(String search, boolean superMatch) {
+        // 当前节点是否匹配
+        boolean currentMatch = superMatch || isMatchSearch(search);
+        if (CollectionUtils.isNotEmpty(children)) {
+            boolean currentMatchBefore = currentMatch;
+            for (QueryFeatureLibraryDto child : children) {
+                boolean childMatch = child.selectSearchMatch(search, currentMatchBefore);
+                if (!currentMatch && childMatch) {
+                    currentMatch = true;
+                }
+            }
+        }
+        setSearchMatch(currentMatch);
+        return currentMatch;
+    }
+
+    private boolean isMatchSearch(String search) {
+        return matchSearch(this.featureCode, search) ||
+                matchSearch(this.displayName, search) ||
+                matchSearch(this.chineseName, search);
+    }
+
+    private boolean matchSearch(String content, String search) {
+        return content != null && content.contains(search);
+    }
 
 }

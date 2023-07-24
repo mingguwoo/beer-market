0b01bbaecc22ce30e2f1c88d87e58b7ec5c9c691
Xiaozhou TU 涂小洲 <xiaozhou.tu@nio.com>
2023-07-24 16:58:32 +0800

    忘记提交的类

---
+++ plm-bom-configuration-domain/src/main/java/com/nio/ngfs/plm/bom/configuration/domain/model/feature/enums/FeatureChangeTypeEnum.java
@@ -0,0 +1,19 @@
+package com.nio.ngfs.plm.bom.configuration.domain.model.feature.enums;
+
+/**
+ * @author xiaozhou.tu
+ * @date 2023/7/24
+ */
+public enum FeatureChangeTypeEnum {
+
+    /**
+     * Feature变更类型
+     */
+    GROUP_ADD,
+    GROUP_EDIT,
+    FEATURE_ADD,
+    FEATURE_EDIT,
+    OPTION_ADD,
+    OPTION_EDIT
+
+}

9a4f6c572972384dd044f6ce110a283c46ed7707
Xiaozhou TU 涂小洲 <xiaozhou.tu@nio.com>
2023-07-26 18:25:35 +0800

    oxo表结构修改

--- sql/boms_oxo_option_package.sql
+++ sql/boms_oxo_option_package.sql
@@ -1,7 +1,7 @@
-CREATE TABLE `boms_oxo_option_package_info` (
+CREATE TABLE `boms_oxo_option_package` (
   `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `row_id` bigint NOT NULL COMMENT 'oxo行id',
-  `base_vehicle_id` bigint NOT NULL COMMENT 'oxo列id',
+  `feature_option_id` bigint NOT NULL COMMENT 'oxo行id',
+  `base_vehicle_id` varchar(11) NOT NULL COMMENT 'oxo列id',
   `package_code` varchar(32) NOT NULL DEFAULT '' COMMENT 'Default ●  Unavailable - Available ○',
   `description` varchar(255) DEFAULT NULL COMMENT '描述',
   `brand` varchar(32) NOT NULL COMMENT '品牌',
@@ -11,7 +11,6 @@ CREATE TABLE `boms_oxo_option_package_info` (
   `update_user` varchar(32)  NOT NULL DEFAULT '' COMMENT '更新者',
   `del_flag` smallint NOT NULL DEFAULT '0' COMMENT '是否删除 1删除 0未删除',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `uniq_key` (`row_id`,`base_vehicle_id`) USING BTREE,
-  KEY `idx_row_id` (`row_id`) USING BTREE,
+  UNIQUE KEY `uniq_key` (`feature_option_id`,`base_vehicle_id`) USING BTREE,
   KEY `idx_base_vehicle_id` (`base_vehicle_id`) USING BTREE
 ) ENGINE=InnoDB COMMENT='oxo 打点信息表';

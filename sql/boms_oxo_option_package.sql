e1b15398d35ed6407578a268523f109382087b86
Xiaozhou TU 涂小洲 <xiaozhou.tu@nio.com>
2023-07-26 19:28:23 +0800

    更新表字段

--- sql/boms_oxo_option_package.sql
+++ sql/boms_oxo_option_package.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `boms_oxo_option_package` (
   `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键',
   `feature_option_id` bigint NOT NULL COMMENT 'oxo行id',
-  `base_vehicle_id` varchar(11) NOT NULL COMMENT 'oxo列id',
+  `base_vehicle_id` bigint NOT NULL COMMENT 'oxo列id',
   `package_code` varchar(32) NOT NULL DEFAULT '' COMMENT 'Default ●  Unavailable - Available ○',
   `description` varchar(255) DEFAULT NULL COMMENT '描述',
   `brand` varchar(32) NOT NULL COMMENT '品牌',

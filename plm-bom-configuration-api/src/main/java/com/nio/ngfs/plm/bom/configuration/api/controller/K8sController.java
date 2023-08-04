a080d12c9316fa62c6dec78612d763a18e4f07eb
luke.zhu <luke.zhu@nio.com>
2023-08-04 11:48:09 +0800

    优化eureka优雅下线.

--- plm-bom-configuration-api/src/main/java/com/nio/ngfs/plm/bom/configuration/api/controller/K8sController.java
+++ plm-bom-configuration-api/src/main/java/com/nio/ngfs/plm/bom/configuration/api/controller/K8sController.java
@@ -1,11 +1,10 @@
 package com.nio.ngfs.plm.bom.configuration.api.controller;
 
 import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.actuate.endpoint.web.annotation.RestControllerEndpoint;
-import org.springframework.cloud.netflix.eureka.serviceregistry.EurekaRegistration;
-import org.springframework.cloud.netflix.eureka.serviceregistry.EurekaServiceRegistry;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -18,23 +17,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequiredArgsConstructor
 public class K8sController {
 
-    private final EurekaServiceRegistry eurekaServiceRegistry;
-    private final EurekaRegistration eurekaRegistration;
+    private final EurekaClient eurekaClient;
 
     @GetMapping("/preStop")
     public void preStop() {
-        try {
+        try{
             //只有当服务注册到eureka上面的时候才graceful shutdown
-            String status = eurekaRegistration.getEurekaClient().getInstanceRemoteStatus().toString();
-            if (InstanceInfo.InstanceStatus.UP.toString().equals(status)) {
-                eurekaServiceRegistry.setStatus(eurekaRegistration, InstanceInfo.InstanceStatus.DOWN.toString());
-                //触发本地状态更新
-                eurekaServiceRegistry.deregister(eurekaRegistration);
-            } else {
-                log.warn("eureka has not been registered,pls check the config");
+            String status = eurekaClient.getInstanceRemoteStatus().toString();
+            if(InstanceInfo.InstanceStatus.UP.toString().equals(status)){
+                eurekaClient.shutdown();
+            }else{
+                log.warn("Eureka client is NOT up, please check the configuration...");
             }
-        } catch (Exception e) {
-            log.error("eureka down failed,msg is {}", e.getMessage());
+        }catch (Exception e){
+            log.error("Eureka down is failed, the message is {}",e.getMessage());
         }
+        log.info("Stop eureka successfully.");
     }
 }

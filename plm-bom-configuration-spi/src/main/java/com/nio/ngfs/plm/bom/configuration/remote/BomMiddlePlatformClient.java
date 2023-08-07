10362d9b2a647c703eb9a687d93148824196f09e
luke.zhu <luke.zhu@nio.com>
2023-08-07 12:03:42 +0800

    feign走eureka调用

--- plm-bom-configuration-spi/src/main/java/com/nio/ngfs/plm/bom/configuration/remote/BomMiddlePlatformClient.java
+++ plm-bom-configuration-spi/src/main/java/com/nio/ngfs/plm/bom/configuration/remote/BomMiddlePlatformClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
  * @author luke.zhu
  * @date 07/11/2023
  */
-@FeignClient(url = "${url.middle}", name = "plm-bom-middle-platform")
+@FeignClient(name = "plm-bom-middle-platform")
 public interface BomMiddlePlatformClient {
 
 
--- plm-bom-configuration-spi/src/main/java/com/nio/ngfs/plm/bom/configuration/remote/ColorPartClient.java
+++ plm-bom-configuration-spi/src/main/java/com/nio/ngfs/plm/bom/configuration/remote/ColorPartClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
  * @author luke.zhu
  * @date 07/11/2023
  */
-@FeignClient(url = "${url.color}", name = "plm-bom-color")
+@FeignClient(name = "plm-bom-color")
 public interface ColorPartClient {
 
 

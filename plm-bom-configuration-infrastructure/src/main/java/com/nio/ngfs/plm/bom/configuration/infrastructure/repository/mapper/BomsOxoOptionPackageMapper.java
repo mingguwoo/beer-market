4772d17b5ea80579bc434bc67266445214bf1013
wangchao <wangchao.wang@nio.com>
2023-08-09 11:11:57 +0800

    初始化 oxo

--- plm-bom-configuration-infrastructure/src/main/java/com/nio/ngfs/plm/bom/configuration/infrastructure/repository/mapper/BomsOxoOptionPackageMapper.java
+++ plm-bom-configuration-infrastructure/src/main/java/com/nio/ngfs/plm/bom/configuration/infrastructure/repository/mapper/BomsOxoOptionPackageMapper.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.nio.ngfs.plm.bom.configuration.infrastructure.repository.entity.BomsOxoOptionPackageEntity;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+
 /**
  * <p>
  * oxo 打点信息表 Mapper 接口
@@ -17,6 +19,6 @@ public interface BomsOxoOptionPackageMapper extends BaseMapper<BomsOxoOptionPack
 
 
 
-    void insertOxoOptionPackages(@Param("oxoOptionPackages") BomsOxoOptionPackageEntity entity);
+    void insertOxoOptionPackages(@Param("oxoOptionPackages") List<BomsOxoOptionPackageEntity> entity);
 
 }

4d6f6d287259fc632b6be2a4651dc12d98d4a2ae
Xiaozhou TU 涂小洲 <xiaozhou.tu@nio.com>
2023-11-03 15:46:21 +0800

    bug报错修复

--- plm-bom-configuration-infrastructure/src/main/java/com/nio/ngfs/plm/bom/configuration/infrastructure/repository/converter/ConfigurationRuleConverter.java
+++ plm-bom-configuration-infrastructure/src/main/java/com/nio/ngfs/plm/bom/configuration/infrastructure/repository/converter/ConfigurationRuleConverter.java
@@ -1,5 +1,6 @@
 package com.nio.ngfs.plm.bom.configuration.infrastructure.repository.converter;
 
+import com.google.common.collect.Lists;
 import com.nio.ngfs.plm.bom.configuration.domain.model.configurationrule.ConfigurationRuleAggr;
 import com.nio.ngfs.plm.bom.configuration.infrastructure.repository.converter.common.MapStructDataConverter;
 import com.nio.ngfs.plm.bom.configuration.infrastructure.repository.converter.mapping.ConfigurationRuleMapper;
@@ -17,6 +18,11 @@ public class ConfigurationRuleConverter implements MapStructDataConverter<Config
     @Override
     public MapstructMapper<ConfigurationRuleAggr, BomsConfigurationRuleEntity> getMapper() {
         return ConfigurationRuleMapper.INSTANCE;
+    }
+
+    @Override
+    public void convertEntityToDoCallback(BomsConfigurationRuleEntity entity, ConfigurationRuleAggr domainObject) {
+        domainObject.setOptionList(Lists.newArrayList());
     }
 
 }
